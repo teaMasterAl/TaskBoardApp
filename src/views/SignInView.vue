17dc48d9be3bac2bc36a2d8b82487bad5d8126e8
teaMasterAl <bushnev.site@gmail.com>
2023-07-02 16:49:59 +0300

    Added two view for registration and authorization

---
+++ src/views/SignInView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Авторизация</h2>
+    <div>
+      <input type="text" placeholder="Логин" v-model="login" />
+      <input type="password" placeholder="Пароль" v-model="password" />
+      <button @click="handleSignUp">Войти</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { signIn } from '@/api/auth'
+
+const login = ref('')
+const password = ref('')
+
+const handleSignUp = async () => {
+  const result = await signIn({
+    login: login.value,
+    password: password.value
+  })
+  console.log("result", result)
+}
+</script>
+
+<style scoped></style>
