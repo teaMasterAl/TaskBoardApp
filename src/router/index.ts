9f8b2f03e7be4151f2a4f389c63cea96ca1de6b2
teaMasterAl <bushnev.site@gmail.com>
2023-07-16 20:23:24 +0300

    fix url

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import { routes } from './routes'
 import {useAuthStore} from "~/stores/auth";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_API_BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
